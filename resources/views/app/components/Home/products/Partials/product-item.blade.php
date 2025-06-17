337e4e1d84d1b79dfde28b116bdae385d5d6fc98
RakibulIslam <rakibulislamsofficial.com>
2025-06-17 14:02:46 +0600

    product image height

--- resources/views/app/components/Home/products/Partials/product-item.blade.php
+++ resources/views/app/components/Home/products/Partials/product-item.blade.php
@@ -4,7 +4,7 @@
         <img 
             src="{{ $product->FOTO ? tenant_asset($product->FOTO) : 'https://psediting.websites.co.in/obaju-turquoise/img/product-placeholder.png' }}" 
             alt="{{ $product['DESCRIZIONEBREVE'] }}" 
-            class="h-full object-contain relative z-10 drop-shadow-2xl"
+            class="h-[200px] w-full object-cover object-center relative z-10 drop-shadow-2xl"
         />
     </div>
 
