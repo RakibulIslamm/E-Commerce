6f1ee556ae848ff1c9266b6143a28394605fef87
Larens94 <fabrizio.corpora@gmail.com>
2025-07-07 12:14:55 +0200

    add text "nessuna immagine" in placehold.co

--- resources/views/app/components/Home/categoris/Partials/category-item.blade.php
+++ resources/views/app/components/Home/categoris/Partials/category-item.blade.php
@@ -2,7 +2,7 @@
     class="relative h-[100px] w-[500px] group rounded-lg overflow-hidden transition-all ease-in-out border swiper-slide bg-slate-300">
     {{-- @dd($category->img); --}}
     <img class="rounded-lg relative z-0 h-full object-cover w-full object-center"
-        src="{{ isset($category->img) ? tenant_asset($category->img) : 'https://placehold.co/500x100' }}"
+        src="{{ isset($category->img) ? tenant_asset($category->img) : 'https://placehold.co/500x100?text=Nessuna%20immagine' }}"
         alt="">
     <h4
         class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg text-white font-bold z-20 drop-shadow-2xl">
