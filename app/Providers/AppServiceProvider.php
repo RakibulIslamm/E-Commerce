8999a7da4d46bdf73f3605d433a879a6f3ac6bae
Larens94 <fabrizio.corpora@gmail.com>
2025-07-07 17:06:36 +0200

    test004

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Category;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,41 +23,63 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('*', function ($view) {
+            $startTime = microtime(true);
+
             $requestDomain = request()->getHost();
             $centralDomains = config('tenancy.central_domains', []);
-            // dd($view->exception);
+            Log::info("View composer started", ['domain' => $requestDomain]);
 
             if (!$view->exception) {
                 if (in_array($requestDomain, $centralDomains)) {
+                    Log::info("Domain is central domain", ['domain' => $requestDomain]);
                     $view->with([
                         'user' => auth()->user()
                     ]);
                 } else {
+                    Log::info("Domain is tenant domain, fetching tenant data", ['domain' => $requestDomain]);
                     $tenant = tenant();
                     $user = auth()->user();
-                    $categories = Category::with('children')->whereNull('parent_id')->usedInProducts()->orderBy('nome')->get();
+
+                    $categoriesStart = microtime(true);
+                    $categories = Category::with('children')
+                        ->whereNull('parent_id')
+                        ->usedInProducts()
+                        ->orderBy('nome')
+                        ->get();
+                    $categoriesDuration = microtime(true) - $categoriesStart;
+                    Log::info("Categories fetched", ['count' => $categories->count(), 'duration_seconds' => $categoriesDuration]);
+
                     if (isset($tenant->data) && $tenant->data != null) {
                         $tenant->data = json_decode($tenant->data);
+                        Log::info("Tenant data decoded");
                     }
 
                     $hide_catalogo_mandatory = $tenant->registration_process == 'Mandatory' && !$user;
-
                     $hide_catalogo_mandatory_con_conferma = $tenant->registration_process == 'Mandatory with confirmation' && !$user?->email_verified_at;
-
                     $email_verified = $user?->email_verified_at ? true : false;
+
+                    Log::info("User and tenant flags", [
+                        'user_id' => $user?->id,
+                        'hide_catalogo_mandatory' => $hide_catalogo_mandatory,
+                        'hide_catalogo_mandatory_con_conferma' => $hide_catalogo_mandatory_con_conferma,
+                        'email_verified' => $email_verified,
+                    ]);
 
                     $view->with([
                         'user' => $user,
-                        'site_settings' =>
-                            $tenant,
+                        'site_settings' => $tenant,
                         'categories' => $categories,
                         'hide_catalogo_mandatory_con_conferma' => $hide_catalogo_mandatory_con_conferma,
                         'hide_catalogo_mandatory'=> $hide_catalogo_mandatory,
                         'email_verified' => $email_verified
                     ]);
                 }
+            } else {
+                Log::warning("View exception detected", ['exception' => $view->exception]);
             }
 
+            $totalDuration = microtime(true) - $startTime;
+            Log::info("View composer finished", ['duration_seconds' => $totalDuration]);
         });
     }
 }
