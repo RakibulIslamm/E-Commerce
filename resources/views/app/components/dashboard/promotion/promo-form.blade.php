de17a54fa7a1ca19cc227e8f786d485adb700dac
Larens94 <fabrizio.corpora@gmail.com>
2025-07-10 14:19:41 +0200

    traslate promotion

--- resources/views/app/components/dashboard/promotion/promo-form.blade.php
+++ resources/views/app/components/dashboard/promotion/promo-form.blade.php
@@ -7,7 +7,7 @@
 
     <div class="w-full flex items-center gap-3">
         <div class="w-full">
-            <label for="slide_name" class="block text-gray-700 text-sm font-bold mb-2">Promo Name</label>
+            <label for="slide_name" class="block text-gray-700 text-sm font-bold mb-2">Nome</label>
             <input id="slide_name" name="name" type="text" value="{{ old('name', $promotion['name'] ?? '') }}"
                 required
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
@@ -26,7 +26,7 @@
     </div>
     <div class="w-full flex items-center gap-3">
         <div class="w-full mt-4">
-            <label for="discount_amount" class="block text-gray-700 text-sm font-bold mb-2">Discount Amount</label>
+            <label for="discount_amount" class="block text-gray-700 text-sm font-bold mb-2">Sconto</label>
             <input id="discount_amount" name="discount_amount" type="number" step="0.01" min="0"
                 value="{{ old('discount_amount', $promotion['discount_amount'] ?? '') }}"
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
@@ -34,8 +34,7 @@
         </div>
 
         <div class="w-full mt-4">
-            <label for="discount_percentage" class="block text-gray-700 text-sm font-bold mb-2">Discount
-                Percentage</label>
+            <label for="discount_percentage" class="block text-gray-700 text-sm font-bold mb-2">Percentuale sconto</label>
             <input id="discount_percentage" name="discount_percentage" type="number" min="0" max="100"
                 value="{{ old('discount_percentage', $promotion['discount_percentage'] ?? '') }}"
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
@@ -53,7 +52,7 @@
 
     <div class="w-full flex items-center gap-3">
         <div class="w-full mt-4">
-            <label for="start_date" class="block text-gray-700 text-sm font-bold mb-2">Start Date</label>
+            <label for="start_date" class="block text-gray-700 text-sm font-bold mb-2">Data inizio</label>
             <input id="start_date" name="start_date" type="date"
                 value="{{ old('start_date', $promotion['start_date'] ?? null) }}" required
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
@@ -61,7 +60,7 @@
         </div>
 
         <div class="w-full mt-4">
-            <label for="end_date" class="block text-gray-700 text-sm font-bold mb-2">End Date</label>
+            <label for="end_date" class="block text-gray-700 text-sm font-bold mb-2">Data fine</label>
             <input id="end_date" name="end_date" type="date"
                 value="{{ old('end_date', $promotion['end_date'] ?? '') }}" required
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
@@ -70,7 +69,7 @@
     </div>
 
     <div class="w-full mt-4">
-        <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Description</label>
+        <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Descrizione</label>
         <textarea id="description" name="description"
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             rows="3">{{ old('description', $promotion['description'] ?? '') }}</textarea>
@@ -86,15 +85,15 @@
         <label id="upload-promo-bg-input"
             class="px-5 py-2 border rounded-md cursor-pointer flex items-center justify-start gap-2 w-max">
             <x-lucide-image-up class="w-5 h-5" />
-            Upload background Image
+            Carica immagine di sfondo
             <input accept="image/*" class="sr-only" type="file" name="bg_image" id="upload-slide-input">
         </label>
-        <span class="text-sm text-red-800 block mt-2">Image size 1920x1080</span>
+        <span class="text-sm text-red-800 block mt-2">Dimensioni dell'immagine 1920x1080</span>
         <img id="promo-image-preview" src="{{ $promotion['bg_image'] ?? '' }}"
             class="mt-3 w-full object-cover object-center" alt="">
     </div>
 
-    <button type="submit" class="px-5 py-1 border rounded bg-sky-500 text-white">Create</button>
+    <button type="submit" class="px-5 py-1 border rounded bg-sky-500 text-white">Crea</button>
 </form>
 
 
