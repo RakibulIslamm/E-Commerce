ecfeacf4dea4e611354fa152272c1d9421dd654f
Larens94 <fabrizio.corpora@gmail.com>
2025-07-10 14:41:03 +0200

    remove limit order if is zero

--- resources/views/app/pages/cart/index.blade.php
+++ resources/views/app/pages/cart/index.blade.php
@@ -85,8 +85,8 @@
         }, 0);
         
         // Update the shipping limit message based on the cart total
-        if (cartTotal >= shippingSetting.minimumOrder) {
-            if (shippingSetting.minimumOrder != 0) {
+        if (shippingSetting.minimumOrder != 0) {
+            if (cartTotal >= shippingSetting.minimumOrder) {
                 shippingLimitP.className = "font-semibold bg-green-500 py-1 px-3 rounded-md mb-5 lg:-mt-10 flex items-center text-green-700 bg-opacity-50";
                 shippingLimitP.innerHTML = `
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="w-5 h-5 mr-2">
@@ -94,15 +94,15 @@
                     </svg>
                     Limite minimo di ordine raggiunto: ${shippingSetting.minimumOrder}€
                 `;
+            } else if (shippingSetting.minimumOrder > 0) {
+                shippingLimitP.className = "font-semibold text-red-800 bg-red-200 py-1 px-3 rounded-md mb-5 lg:-mt-10";
+                shippingLimitP.innerHTML = `Limite minimo di ordine: ${shippingSetting.minimumOrder}€`;
+            } else {
+                shippingLimitP.className = "font-semibold text-red-800 bg-red-200 py-1 px-3 rounded-md mb-5 lg:-mt-10";
+                shippingLimitP.innerHTML = "Non ci sono metodi di spedizione disponibili su questa piattaforma.";
             }
-           
-        } else if (shippingSetting.minimumOrder > 0) {
-            shippingLimitP.className = "font-semibold text-red-800 bg-red-200 py-1 px-3 rounded-md mb-5 lg:-mt-10";
-            shippingLimitP.innerHTML = `Limite minimo di ordine: ${shippingSetting.minimumOrder}€`;
-        } else {
-            shippingLimitP.className = "font-semibold text-red-800 bg-red-200 py-1 px-3 rounded-md mb-5 lg:-mt-10";
-            shippingLimitP.innerHTML = "Non ci sono metodi di spedizione disponibili su questa piattaforma.";
         }
+       
     }
 
     // function getCartTotal() {
